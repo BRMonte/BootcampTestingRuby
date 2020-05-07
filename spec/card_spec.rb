0cc65883e4ac46bef81ff497ca56f754fc31eea2
bruno montebello <brunormontebello@hotmail.com>
2020-05-07 06:35:49 -0300

    Create the class Card + inicitalize the instance

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,10 +1,19 @@
-Rspec.describe 'Card' do #a method for describing what we are testing #it can teke a string as an argument; the class it self; a method etc
+class Card
+  def initialize(type)
+  end
+end
 
-  it "has a type" do#this string argument should be a description of what i'm testing. not a description of HOW, bout of WHAT IS
+RSpec.describe 'Card' do
+
+  it "has a type" do
     card = Card.new("Ace of Spades")
-    expect(card.type).to eq("Ace of Spades") #ep it is a method, means EQUAL
+    expect(card.type).to eq("Ace of Spades")
   end
 end
 
 #what DESCRIBE creates is known as an EXAMPLE GROUP'= group of related testes
 # IT can be replaced by SPECIFY
+#this string argument should be a description of what i'm testing. not a description of HOW, bout of WHAT IS
+#a method for describing what we are testing #it can teke a string as an argument; the class it self; a method etc
+#ep it is a method, means EQUAL
+#each IT method creates 1 EXAMPLE
