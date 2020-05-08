edea6395112ae1f09d4229faa2ed384732884219
bruno montebello <brunormontebello@hotmail.com>
2020-05-08 18:33:11 -0300

    Fix typos

--- spec/language_spec.rb
+++ spec/language_spec.rb
@@ -1,8 +1,8 @@
 # best practice to DRY test code
-# MEMORIZATION: it caches results just gotten from a previous process.
-#with MEMORIZATION the process only happens once.
+# MEMOIZATION: it caches results just gotten from a previous process.
+#with MEMOIZATION the process only happens once.
 # And if you need that result more than once, it will give it right away. It wont make the same calculations again
-#MEMORIZATION CACHES RESULTS
+#MEMOIZATION CACHES RESULTS
 
 #LET method: avaiable in RSpec like IT, DESCRIBE, EXPECT etc
 # it takes an argument (:SYMBOL)
@@ -21,16 +21,21 @@ end
 
 RSpec.describe Language do
 
-  let(:language) {Language.new("Portugues", "Latin")} #MEMORIZATION syntax
+  let(:language) {Language.new("Portugues", "Latin")}
+  # MEMOIZATION syntax. #instantiate Language and assing it to :language for EACH example we have
+  # let is a LAZY PROCESS: it only instantiates IF Language.new is needed
+  # in  the "before_action" cenario, it will ALWAYS instantiates, even when Language is not needed
+  # the object will persist and be cached within the SAME example. Across different examples, RSpec will evaluate the let block again to ensure isolation between tests.
 
   it "has a name" do
-    expect(language.name).to eq("Portugues")
-    language.name = "Frances"
+    expect(language.name).to eq("Portugues") #checks the instance created
+    language.name = "Frances" # reassigns the SAME instance created before (:language)
     expect(language.name).to eq("Frances")
   end
 
   it "belongs to a family" do
-    expect(language.family).to eq("Latin")
+    language_family = "Latin"
+    expect(language.family).to eq(language_family)
   end
 
 end
