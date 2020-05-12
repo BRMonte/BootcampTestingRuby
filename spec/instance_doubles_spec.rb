5023dce1528ae3dbe44051c9afce692c8cb3419c
bruno montebello <brunormontebello@hotmail.com>
2020-05-12 14:25:19 -0300

    Add card_double_spec example

---
+++ spec/instance_doubles_spec.rb
@@ -0,0 +1,23 @@
+class Person
+  def a
+    sleep(3)
+    "hello"
+  end
+end
+
+RSpec.describe Person do
+  describe "regular double" do
+    it "can implement any method" do
+      person = double(a: "hello", b:  20)
+      expect(person.a).to eq("hello")
+    end
+  end
+
+  describe "instance double" do
+    it "can only implement methods that are defined on the class" do
+      person = instance_double(Person)
+      allow(person).to receive(:a).with(3, 10).and_return("hello") # will fail
+    end
+  end
+
+end
