def4f6baa273da12baa96fabbc322d626c2476a4
bruno montebello <brunormontebello@hotmail.com>
2020-05-11 23:05:42 -0300

    Add movie_spec example

--- spec/double_spec.rb
+++ spec/double_spec.rb
@@ -10,8 +10,8 @@ RSpec.describe "a random double" do
     # allow(stuntman).to receive(:fall_of_ladder).and_return("Ouch")
     # expect(stuntman.fall_of_ladder).to eq("Ouch")
 
-    stuntman = double("Mr. Danger")
-    allow(stuntman).to receive_messages(fall_of_ladder: "Ouch", light_on_fire: true)
+    stuntman = double("Mr. Danger") #double method
+    allow(stuntman).to receive_messages(fall_of_ladder: "Ouch", light_on_fire: true) # allow give Mr Danger the methods fall_of_ladder and light_on_fire
     expect(stuntman.fall_of_ladder).to eq("Ouch")
     expect(stuntman.light_on_fire).to eq(true)
   end
